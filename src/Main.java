a4c870cd852230e5f29942d626c7ceea5e2db534
Daksanya <daksanyanandhakumar@gmail.com>
2023-07-11 16:27:54 +0530

    Second commit done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,89 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+class Account {
+   private double balance;
+   public void setBalance(double amount){
+       this.balance=amount;
+   }
+   public double getBalance(){
+       return this.balance;
+   }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public void deposit(double amount) throws InvalidAmountException{
+        if(amount<=0){
+            throw new InvalidAmountException("Please Enter a valid Amount");
         }
+
+        balance+=amount;
+        System.out.println("Deposit successful, New Balance is:"+balance);
+    }
+    public void withdraw(double amount)  throws InsufficientFundsException,InvalidAmountException{
+        if(amount>balance){
+            throw new InsufficientFundsException("Insufficient bank balance");
+    }
+        if(amount<=0){
+            throw new InvalidAmountException("Please Enter a valid Amount");
+        }
+        balance-=amount;
+        System.out.println("Withdraw successful, New Balance is:"+balance);
+
+}}
+class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String errorMessage){
+        super(errorMessage);
     }
 }
+class InvalidAmountException extends Exception{
+    public InvalidAmountException(String errorMessage){
+        super(errorMessage);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.setBalance(1000.0);
+        double amount;
+        Scanner sc = new Scanner(System.in);
+        boolean t = true;
+        while(t){
+            System.out.println("Enter 1 to Deposit Amount");
+            System.out.println("Enter 2 to Withdraw Amount");
+            System.out.println("Enter 3 to Check Account Balance");
+            System.out.println("Enter 4 to Exit");
+            int option=sc.nextInt();
+        switch (option){
+            case 1:
+                System.out.println("Enter Amount to Deposit:");
+                amount=sc.nextDouble();
+                try{
+                    acc.deposit(amount);
+                }catch(InvalidAmountException exception) {
+                    System.out.println(exception.getMessage());
+                }break;
+            case 2:
+                System.out.println("Enter Amount to Withdraw");
+                amount=sc.nextDouble();
+                try{
+                    acc.withdraw(amount);
+                }catch(InvalidAmountException exception){
+                    System.out.println(exception.getMessage());
+                }
+                catch (InsufficientFundsException exception){
+                    System.out.println(exception.getMessage());
+                }
+                break;
+            case 3:
+                System.out.println("Your Account Balance is:"+acc.getBalance());
+                break;
+            case 4:
+                t=false;
+                break;
+            default:
+                System.out.println("Enter a valid option");
+
+        }}
+
+
+    }}
+
+
